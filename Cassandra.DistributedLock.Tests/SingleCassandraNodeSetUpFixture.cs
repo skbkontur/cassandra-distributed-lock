8e32b30ef752703d5e026d7f2cc8aff0a51b00cb
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-04-26 12:31:04 +0400

    use gh actions

--- Cassandra.DistributedLock.Tests/SingleCassandraNodeSetUpFixture.cs
+++ Cassandra.DistributedLock.Tests/SingleCassandraNodeSetUpFixture.cs
@@ -1,12 +1,11 @@
 using System;
-using System.IO;
+using System.Linq;
 using System.Net;
 
 using Cassandra.DistributedLock.Tests.Logging;
 
 using NUnit.Framework;
 
-using SkbKontur.Cassandra.Local;
 using SkbKontur.Cassandra.ThriftClient.Abstractions;
 using SkbKontur.Cassandra.ThriftClient.Clusters;
 using SkbKontur.Cassandra.ThriftClient.Schema;
@@ -20,16 +19,6 @@ namespace Cassandra.DistributedLock.Tests
         public static void SetUp()
         {
             Log4NetConfiguration.InitializeOnce();
-            var templateDirectory = Path.Combine(FindCassandraTemplateDirectory(AppDomain.CurrentDomain.BaseDirectory), @"v3.11.x");
-            var deployDirectory = Path.Combine(Path.GetTempPath(), "deployed_cassandra_v3.11.x");
-            node = new LocalCassandraNode(templateDirectory, deployDirectory)
-                {
-                    RpcPort = 9360,
-                    CqlPort = 9343,
-                    JmxPort = 7399,
-                    GossipPort = 7400,
-                };
-            node.Restart(timeout : TimeSpan.FromMinutes(1));
 
             var logger = Log4NetConfiguration.RootLogger.ForContext(nameof(SingleCassandraNodeSetUpFixture));
             cassandraCluster = new CassandraCluster(CreateCassandraClusterSettings(), logger);
@@ -56,20 +45,22 @@ namespace Cassandra.DistributedLock.Tests
                 }, changeExistingKeyspaceMetadata : false);
         }
 
-        [OneTimeTearDown]
-        public static void TearDown()
-        {
-            node.Stop();
-        }
-
         public static ICassandraClusterSettings CreateCassandraClusterSettings(int attempts = 5, TimeSpan? timeout = null)
         {
-            return new SingleNodeCassandraClusterSettings(new IPEndPoint(IPAddress.Parse(node.RpcAddress), node.RpcPort))
+            return new SingleNodeCassandraClusterSettings(new IPEndPoint(GetIpV4Address("127.0.0.1"), 9160))
                 {
-                    ClusterName = node.ClusterName,
+                    ClusterName = "TestCluster",
                     Attempts = attempts,
                     Timeout = (int)(timeout ?? TimeSpan.FromSeconds(6)).TotalMilliseconds,
                 };
+        }
+
+        private static IPAddress GetIpV4Address(string hostNameOrIpAddress)
+        {
+            if (IPAddress.TryParse(hostNameOrIpAddress, out var res))
+                return res;
+
+            return Dns.GetHostEntry(hostNameOrIpAddress).AddressList.First(address => !address.ToString().Contains(':'));
         }
 
         public static void TruncateAllColumnFamilies()
@@ -77,20 +68,9 @@ namespace Cassandra.DistributedLock.Tests
             cassandraCluster.RetrieveColumnFamilyConnection(RemoteLockKeyspace, RemoteLockColumnFamily).Truncate();
         }
 
-        private static string FindCassandraTemplateDirectory(string currentDir)
-        {
-            if (currentDir == null)
-                throw new Exception("Невозможно найти каталог с Cassandra-шаблонами");
-            var cassandraTemplateDirectory = Path.Combine(currentDir, cassandraTemplates);
-            return Directory.Exists(cassandraTemplateDirectory) ? cassandraTemplateDirectory : FindCassandraTemplateDirectory(Path.GetDirectoryName(currentDir));
-        }
-
         public const string RemoteLockKeyspace = "TestRemoteLockKeyspace";
         public const string RemoteLockColumnFamily = "TestRemoteLockCf";
 
-        private const string cassandraTemplates = @"cassandra-local\cassandra";
-
-        private static LocalCassandraNode node;
         private static CassandraCluster cassandraCluster;
     }
 }
